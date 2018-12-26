3b07e8df18a3c715b9faef0636e8e9ec707a861e
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-26 17:29:00 +0000

    returns NAN when trying to divide by 0

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -76,6 +76,15 @@ describe('calculator functionality', function() {
     element(by.css('#number9')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('531441');
-  })
+  });
+
+  it('should return NaN when trying to divide a number by zero', function(){
+  running_total = element(by.css('#running_total'));
+  element(by.css('#number5')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('NaN');
+});
 
 });
