afb31a176a33931182e508b0886d8ff8dd844e8c
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-26 17:18:02 +0000

    can deal with large numbers

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -52,4 +52,30 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('0.75');
   })
 
+  it('should be able to deal with negative numbers', function () {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-56');
+  })
+
+  it('should be able to deal with very large numbers', function () {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number9')).click();    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('531441');
+  })
+
 });
