87c96aa1aeee448bf02679655c2bdbed1c750658
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-26 16:10:15 +0000

    dividing mocha test passing

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -9,7 +9,7 @@ describe('calculator', function () {
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
-  })
+  });
 
   it('it should be able to add numbers', function() {
     calculator.numberClick(1);
@@ -17,7 +17,7 @@ describe('calculator', function () {
     calculator.add(4);
     actual = calculator.runningTotal;
     assert.equal(actual, 5);
-  })
+  });
 
   it('it should be able to subtract numbers', function() {
     calculator.numberClick(7);
@@ -25,14 +25,22 @@ describe('calculator', function () {
     calculator.subtract(4);
     actual = calculator.runningTotal;
     assert.equal(actual, 3)
-  })
+  });
 
   it('it should be able to muliply numbers', function () {
     calculator.numberClick(3);
     calculator.operatorClick('*');
     calculator.multiply(5);
     actual = calculator.runningTotal;
-    assert.equal(actual, 15)
-  })
+    assert.equal(actual, 15);
+  });
+
+  it('it should be able to divide numbers', function () {
+    calculator.numberClick(21);
+    calculator.operatorClick('/');
+    calculator.divide(7);
+    actual = calculator.runningTotal;
+    assert.equal(actual, 3);
+  });
 
 });
