a9caabd4d6c92a605a58fa167e0bc932bc5f5fed
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-26 16:40:09 +0000

    passing chaining multiple operations test

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -23,4 +23,12 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('59');
   })
 
+  it('should be able to chain multiple operations together', function () {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
+
 });
